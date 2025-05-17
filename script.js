40358b27a5d5ed70ac3e85d3be1aca43da285260
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-05-17 16:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const changeBtn = document.getElementById('change_button');
+const resetBtn = document.getElementById('Reset');
+
+function resetGridColors(){
+	for (let i = 1; i <= 9; i++) {
+		document.getElementById(i.toString()).style.backgroundColor = "transparent";
+	}
+}
+
+changeBtn.addEventListener('click', () => {
+	const blockId = document.getElementById("block-id").value;
+	const color = document.getElementById("color-id").value;
+
+	resetGridColors();
+
+	const targetedBlock = document.getElementById(blockId);
+
+	if(targetedBlock){
+		targetedBlock.style.backgroundColor = color;
+	} else {
+		alert("Invalid id Please enter id between 1 to 9");
+	}
+});
+
+resetBtn.addEventListener('click', () => {
+	resetGridColors();
+});
